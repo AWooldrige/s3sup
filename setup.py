e017222813ecf6251cf0b0e50b064dc99675ca4d
Alistair Wooldrige <alistair@wooldrige.co.uk>
2019-03-10 20:27:45 +0000

    Specify dev packages to install

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         'boto3>=1,<2',
         'click>=7,<8',
-        'inflect>=2.1,<3',
+        'inflect>=2,<3',
         'jsonschema>=2,<3',
         'requests>=2.21,<3',  # Workaround as boto3 likes latest requests
         'toml>=0.10,<1',
